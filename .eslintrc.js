ce18c2f5163f63f0bf40fc1c0d4e12a5ca7b2c21
Amber Anand <amber.1rn14is015@gmail.com>
2020-08-04 15:32:26 +0530

    project skeleton setup

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 0,
+    'linebreak-style': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        endOfLine: 'auto',
+      },
+    ],
+    'react/prop-types': [
+      1,
+      {
+        ignore: ['context', 'tracking'],
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+};
